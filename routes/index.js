1f955b7d6d1f33ec37edb68b21dd37ca77d716e1
nathanshiaulam <nlam@princeton.edu>
2015-03-22 18:19:23 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  	// if(Parse.User.current()) {
+   		res.render('index', { title: 'OnTheWay' });
+  	// }
+  	// else {
+  	// 	res.redirect('/login');
+  	// }
+});
+
+module.exports = router;
