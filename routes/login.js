1ad646223cc44b7dddc24fa14d329e7b996cd524
nathanshiaulam <nlam@princeton.edu>
2015-04-01 00:48:21 -0400

    coming soon

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	 res.render('login', {title: 'Log In'});
+	 res.render('login', {title: 'ontheway'});
 });
 
 module.exports = router;
