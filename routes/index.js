9d4c8fbc4d2140ad3c051f3177122816f310914e
nathanshiaulam <nlam@princeton.edu>
2015-04-04 16:23:22 -0400

    fixed cover height on mobile

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  	if(Parse.User.current()) {
+  	// if(Parse.User.current()) {
    		res.render('index', { title: 'ontheway' });
-  	}
-  	else {
-  		res.redirect('/login');
-  	}
+  	// }
+  	// else {
+  	// 	res.redirect('/login');
+  	// }
 });
 
 module.exports = router;
